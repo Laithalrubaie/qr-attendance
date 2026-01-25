7938cda9a67194da2c81c0f62e6b66ecd0a2f573
Leo <laith.alrubaie0@gmail.com>
2026-01-25 15:08:33 +0300

    Update record.js

--- api/record.js
+++ api/record.js
@@ -2,7 +2,7 @@
 
 const AIRTABLE_TOKEN = process.env.AIRTABLE_TOKEN;
 const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
-const AIRTABLE_TABLE_NAME = 'Table 1'; 
+const AIRTABLE_TABLE_NAME = 'Table'; 
 
 module.exports = async (req, res) => {
     // CORS Setup
